ae2c5fb4f5ca169807324c68741e80b37baf7edf
Bianca Pinheiro <124315313+ironacon@users.noreply.github.com>
2023-05-22 21:27:26 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const app = express();
 
 app.use(express.json());
 
+/*app.use((req, res, next)=>{
+    console.log("Acessou o middlewares");
+    next();
+});*/
+function valueCont (req, res, next){
+   if(!req.body.email){
+    return res.json({
+        erro: true,
+        mensagem: "necessário enviar email"
+    });
+   };
+   return next();
+}
 app.get("/", (req, res) => (
     res.send("Bem Vinda Bianca")
 ));
@@ -19,12 +32,32 @@ app.get("/contato/:id", (req, res) => {
     });
 });
 
-app.post("/contato", (req, res) => {
+app.post("/contato", valueCont, (req, res) => {
     let {nome} = req.body;
     let {email} = req.body;
     return res.json({
         nome,
         email
+    });
+});
+
+app.put("/contato/:id", valueCont, (req, res) => {
+    const {id} = req.params;
+    const {nome, email} = req.body;
+   
+
+    return res.json({
+        id,
+        nome,
+        email
+    });
+});
+
+app.delete("/contato/:id", (req, res) => {
+    const {id} = req.params;
+
+    return res.json ({
+        id
     });
 });
 app.listen(8080, () => {
